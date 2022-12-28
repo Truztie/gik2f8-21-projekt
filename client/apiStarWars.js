b79ad2ee8f528ecfc03ce143521f707403aee88f
Gabriel Winlöf <h21gabwi@du.se>
2022-12-28 14:05:59 +0100

    82 characters

--- client/apiStarWars.js
+++ client/apiStarWars.js
@@ -1,11 +1,23 @@
-const url = 'https://swapi.dev/api/people/';
-
-
-
+/*const url = 'https://swapi.dev/api/people/';
 
 async function getAll() {
   const result = await fetch(url)
     .then((result) => result.json())
     .catch((e) => e);
   return result;
+}*/
+async function getAll() {
+  const result = [];
+  let url = 'https://swapi.dev/api/people/';
+
+  do {
+    const res = await fetch(url);
+    const { results, next } = await res.json();
+    result.push(...results);
+    url = next;
+    console.log(result);
+  } while(url)
+
+  return result;
+  
 }
