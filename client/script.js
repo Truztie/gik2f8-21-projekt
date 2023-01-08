9c6ddce00b60d0950fb1851bf1caa602fb88215d
Thomas Finneman <thomasfinneman94@gmail.com>
2023-01-08 12:55:48 +0100

    loadingScreen

--- client/script.js
+++ client/script.js
@@ -2,7 +2,29 @@
 let characterList = []
 
 window.addEventListener('load', () => {
-    getAll().then((result) => (characterList = result))
+  // Show the loading screen
+  let html = `<div id="loadingScreen" class="absolute top-0 left-0 right-0 bottom-0 w-full h-full bg-black/50 flex justify-center">
+                <section id="loadingScreen" class="rounded-md border-2 border-slate-900 absolute bg-gradient-to-tr from-teal-400 to-violet-500 m-48 p-5 flex">
+                  <p class="font-bold mr-10">Resources are loading.<br> Please wait until it has been loading. <br> This windows will dissapear when the resources has finishing loading.</p>
+                  <img src="loading.gif" alt="A description of the image" width="100" height="100">
+                </section>
+              </div>`
+  document.getElementById('root').insertAdjacentHTML("beforebegin", html);
+
+   getAll().then((result) => {
+     characterList = result;
+      //Hide the loading screen
+     let loadingScreen = document.getElementById('loadingScreen');
+     loadingScreen.remove();
+   }).catch((error) => {
+     console.log(error)
+     let htmlError = `<div id="loadingScreen" class="absolute top-0 left-0 right-0 bottom-0 w-full h-full bg-black/50 flex justify-center">
+                 <section id="loadingScreen" class="rounded-md border-2 border-slate-900 absolute bg-gradient-to-tr from-teal-400 to-violet-500 m-48 p-5 flex flex-col justify-center ">
+                   <p class="font-bold text-red-700">The page cant fetch the resources right now, try again later!</p>
+                 </section>
+               </div>`
+     document.getElementById('root').insertAdjacentHTML("beforebegin", htmlError);
+   });
 });
 
 searchField.addEventListener('keyup', (e) =>
